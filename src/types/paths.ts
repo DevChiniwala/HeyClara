5271bc617e31340673c6864ba5dc16a92ef57f74
Dev Chiniwala <dev.chiniwala@gmail.com>
2026-07-05 14:31:01 +0530

    feat: add global type definitions and interfaces

---
+++ src/types/paths.ts
@@ -0,0 +1,15 @@
+export interface Paths {
+  home: string;
+  pid: string;
+  daemonLog: string;
+  cronState: string;
+  cronAudit: string;
+  config: string;
+  jobsDir: string;
+  selfDir: string;
+  beadsDir: string;
+  skillsDir: string;
+  imagesDir: string;
+  watchesDir: string;
+  employeesDir: string;
+}
